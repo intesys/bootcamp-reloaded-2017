b65c3e6458368291ebfc53fa569466d245211643
Enrico Costanzi <enrico.costanzi@intesys.it>
2017-05-11 14:08:13 +0200

    dummy portlet

---
+++ modules/bootcamp-portlet/src/main/java/it/intesys/bootcamp/portlet/portlet/BootcampPortlet.java
@@ -0,0 +1,26 @@
+package it.intesys.bootcamp.portlet.portlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author ecostanzi
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=false",
+		"javax.portlet.display-name=Liferay Bootcamp 2017 Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class BootcampPortlet extends MVCPortlet {
+}
