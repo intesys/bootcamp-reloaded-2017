debc8c027b2c4141f8c344cfd295d7e48e97c01e
Enrico Costanzi <enrico.costanzi@intesys.it>
2017-05-12 13:12:39 +0200

    extended original create account action through reference

--- modules/bootcamp-account-jspfragment/src/main/resources/META-INF/resources/create_account.jsp
+++ modules/bootcamp-account-jspfragment/src/main/resources/META-INF/resources/create_account.jsp
@@ -196,14 +196,18 @@ birthdayCalendar.set(Calendar.YEAR, 1970);
 				<liferay-ui:captcha url="<%= captchaURL %>" />
 			</c:if>
 
-
-            <liferay-ui:custom-attribute
+            <aui:input label="Your Quote" name="quote" size="40" type="text" value="" />
+            
+			<!--
+				does not work if you are a guest!
+             <liferay-ui:custom-attribute
                 className="<%= User.class.getName() %>"
                 classPK="<%= 0 %>"
                 editable="<%= true %>"
                 label="<%= true %>"
                 name="quote"
-            />
+                />
+                -->
 
 		</aui:col>
 	</aui:fieldset>
--- modules/bootcamp-account/src/main/java/it/intesys/bootcamp/account/BootcampCreateAccountMVCActionCommand.java
+++ modules/bootcamp-account/src/main/java/it/intesys/bootcamp/account/BootcampCreateAccountMVCActionCommand.java
@@ -6,7 +6,9 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -28,9 +30,30 @@ public class BootcampCreateAccountMVCActionCommand extends BaseMVCActionCommand 
 
 	private Log _log = LogFactoryUtil.getLog(BootcampCreateAccountMVCActionCommand.class);
 
+	private MVCActionCommand originalMVCCommand;
+
 	@Override
 	protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
 		String userQuote = actionRequest.getParameter("ExpandoAttribute--" + "quote" + "--");
-		_log.info(String.format("Hi there! The philosopher here says '%s'. But trying to create the account! :)", userQuote));
+		if(userQuote == null){
+			//the quote hasn't been inserted with a ui:custom-attribute, therefore we look for the simple "quote" attribute
+			// FIXME: 12/05/2017 this way no expando attribute will be set into the user properties
+			userQuote = actionRequest.getParameter("quote");
+		}
+		if(userQuote == null){
+			SessionErrors.add(actionRequest,"wrong-ssn");
+			return;
+		}
+
+		_log.info(String.format("Hi there! The philosopher here says '%s'. Now I can create the account! :)", userQuote));
+		originalMVCCommand.processAction(actionRequest, actionResponse);
+	}
+
+	/**
+	 * https://dev.liferay.com/develop/tutorials/-/knowledge_base/7-0/overriding-mvc-commands
+	 */
+	@Reference(target = "(component.name=com.liferay.login.web.internal.portlet.action.CreateAccountMVCActionCommand)")
+	public void setOriginalMVCCommand(MVCActionCommand originalMVCCommand) {
+		this.originalMVCCommand = originalMVCCommand;
 	}
 }
